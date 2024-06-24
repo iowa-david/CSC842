f1b6d30ee5df21173c2d8304acc6d8fbd2cf81df
iowa-david <148511573+iowa-david@users.noreply.github.com>
2024-06-23 20:29:22 -0500

    Update keyloggerdetection.py

--- keyloggerdetection.py
+++ keyloggerdetection.py
@@ -1,49 +1,50 @@
 import os
 import psutil
 
-keyloggerFiles = [ "keylogger.exe", "logger.exe", "kl.exe"]
-processesNames = [ "keylogger", "logger", "kl"]
+processesNames = [ "keylogger", "keylg", "textinputhost"]
 
 def checkFiles():
-	foundFiles = []
-	cnt = 0
-	for root, dirs, files in os.walk("/"):
-		for file in files:
-			cnt+=1
-			if file in keyloggerFiles:
-				foundFiles.append(os.path.join(root, file))
-	
-	print(f"{cnt} files checked")
-	return foundFiles
-	
+    foundFiles = []
+    cnt = 0
+    for root, dirs, files in os.walk("/"):
+        for file in files:
+            cnt+=1
+            for name in processesNames:
+                if name in str(file):
+                    foundFiles.append(os.path.join(root, file))
+    
+    print(f"{cnt} files checked")
+    return foundFiles
+    
 def checkProcesses():
-	foundProcesses = []
-	for process in psutil.process_iter(['pid', 'name']):
-		if any(proc in process.info['name'].lower() for proc in processesNames):
-			foundProcesses.append(process.info)
-	
-	return foundProcesses
-	
-	
+    foundProcesses = []
+    for process in psutil.process_iter(['pid', 'name']):
+        for target in processesNames:
+            if target in str(process.info['name'].lower()):
+                   foundProcesses.append(process.info)
+    
+    return foundProcesses
+    
+    
 def main():
 
-	foundSuspiciousFiles = checkFiles()
-	
-	foundSuspiciousProcesses = checkProcesses()
-	
-	if(foundSuspiciousFiles):
-		print("Found possible keylogger files")
-		for file in foundSuspiciousFiles:
-			print(f"File found: {file}")
-	else:
-		print("No identified keylogger files found")
-	
-	if(foundSuspiciousProcesses):
-		print("Found possible keylogger process")
-		for process in foundSuspiciousProcesses:
-			print(f"PID: {proc['pid']}, Name: {proc['name']}")
-			
-	else:
-		print("No identified keylogger processes found")
-		
+    foundSuspiciousFiles = checkFiles()
+    
+    foundSuspiciousProcesses = checkProcesses()
+    
+    if(foundSuspiciousFiles):
+        print("Found possible keylogger files")
+        for file in foundSuspiciousFiles:
+            print(f"File found: {file}")
+    else:
+        print("No identified keylogger files found")
+    
+    if(foundSuspiciousProcesses):
+        print("Found possible keylogger process")
+        for process in foundSuspiciousProcesses:
+            print(f"PID: {process['pid']}, Name: {process['name']}")
+            
+    else:
+        print("No identified keylogger processes found")
+        
 main()
